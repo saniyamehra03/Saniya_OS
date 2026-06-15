40f758031fb1ec497195cc95d7416ae389be02db
saniya mehra <mehrasaniya868@gmail.com>
2026-06-15 23:18:23 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    
+    <div>Saniya Os</div>
+      <br/>
+      <div>About Me</div>
+      <div>Projects</div>
+      <div>Placement Tracker</div>
+      <div>Notes</div>
+      <div>Reminder</div>
+      <div>Music Player</div>
+     <div>Taskbar</div>
+    </>
+  )
+}
+
+export default App
