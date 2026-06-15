672e37fd5bec1e509132007f1a6c5763d5122bd8
saniya mehra <mehrasaniya868@gmail.com>
2026-06-16 00:15:06 +0530

    use tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,21 @@ import "./App.css";
 const App = () => {
   return (
     <>
-    
-    <div className="bg-black text-white h-screen">Saniya Os</div>
+   
+    <div className="bg-red-500 text-white h-screen flex flex-col bg-slate-100
+bg-slate-800 font-semibold  pt-4 pb-4 pl-4 pr-4 text-lg">
+      <div>Saniya Os</div>
       <br/>
+      <div className="flex-1">
       <div>About Me</div>
       <div>Projects</div>
       <div>Placement Tracker</div>
       <div>Notes</div>
       <div>Reminder</div>
       <div>Music Player</div>
+      </div>
      <div>Taskbar</div>
+     </div>
     </>
   )
 }
