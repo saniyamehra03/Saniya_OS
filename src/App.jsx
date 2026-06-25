86e6e16be6a9857a0779c36c87c58e34b8093120
saniya mehra <mehrasaniya868@gmail.com>
2026-06-25 23:11:49 +0530

    Replace multiple window conditions with dynamic rendering

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,13 @@ const App = () => {
   ];
 
   const [openApp, setOpenApp] = useState(null);
-
+const appContent={
+  "About Me" : <p>Hello, I'm Saniya</p>,
+  "Projects": <p>My projects will be shown here.</p>,
+  "Notes": <textarea />,
+  "Reminder":<p>Reminder app coming soon.</p>,
+  "Music Player":<p>🎵 Music app coming soon...</p>
+}
   return (
     <div className="h-screen flex flex-col text-white bg-gradient-to-br from-indigo-900 via-purple-900 to-slate-900 p-4 relative">
       <div className="text-2xl font-bold mb-4">
@@ -31,53 +37,16 @@ const App = () => {
           />
         ))}
       </div>
+     {
+      openApp &&(
+        <Window title={openApp}
+        onClose={()=> setOpenApp(null)}
+        >
+          {appContent[openApp]}
+        </Window>
+      )
+     }
 
-      {openApp === "About Me" && (
-           <Window  title = "About Me"
-             onClose={() => setOpenApp(null)}>
-          <p>Hello, I'm Saniya.</p>
-          <p>Frontend Developer & AI Enthusiast.</p>
-          </Window>
-      )}
-
-      {openApp === "Projects" && (
-            <Window title= "Projects"
-            onClose={() => setOpenApp(null)}>
-        <p>My projects will be shown here.</p>
-            </Window>
-      )}
-
-      {openApp === "Placement Tracker" && (
-            <Window title ="Placement Tracker" 
-           onClose={() => setOpenApp(null)}>
-             <p>Placement tracking coming soon.</p>
-           </Window>
-      )}
-
-      {openApp === "Notes" && (
-            <Window title=" Notes"
-           onClose={() => setOpenApp(null)}>
-             <textarea
-            className="border w-full p-2 rounded"
-            rows="6"
-            placeholder="Write your notes..."
-          />
-           </Window>
-      )}
-
-      {openApp === "Reminder" && (
-            <Window title ="Reminder"
-            onClose={() => setOpenApp(null)}>
-                <p>Reminder app coming soon.</p>
-            </Window>
-      )}
-
-      {openApp === "Music Player" && (
-            <Window title ="Music Player"
-             onClose={() => setOpenApp(null)}>
-            <p>🎵 Music app coming soon...</p>
-             </Window>
-      )}
       <div className="absolute bottom-0 left-0 right-0 h-14 bg-black/40 backdrop-blur-md flex items-center justify-between px-4">
         <div>🚀 Start</div>
 
